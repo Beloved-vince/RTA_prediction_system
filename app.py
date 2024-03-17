ef8f678fca7ee2635ebf40a6ffafc8cd7ee28a44
Beloved-vince <vinceoludare@gmail.com>
2024-03-17 21:26:49 +0100

    'update'

--- app.py
+++ app.py
@@ -91,11 +91,7 @@ def main():
               else:
                      st.write(f"The severity prediction is slight injury")
                
-              st.write("Developed By: Avi kumar Talaviya")
-              st.markdown("""Reach out to me on: [Twitter](https://twitter.com/avikumart_) |
-              [Linkedin](https://www.linkedin.com/in/avi-kumar-talaviya-739153147/) |
-              [Kaggle](https://www.kaggle.com/avikumart) 
-              """)
+
 
 
 a,b,c = st.columns([0.2,0.6,0.2])
@@ -111,7 +107,7 @@ All the sensitive information has been excluded during data encoding and finally
 Then it is preprocessed and for identification of major causes of the accident by analyzing it using different machine learning classification algorithms.
 """)
 
-st.markdown("Source of the dataset: [Click Here](https://www.narcis.nl/dataset/RecordID/oai%3Aeasy.dans.knaw.nl%3Aeasy-dataset%3A191591)")
+st.markdown("Source of the dataset: [Click Here](https://www.kaggle.com/datasets/saurabhshahane/road-traffic-accidents)")
 
 st.subheader("🧭 Problem Statement:")
 st.text("""The target feature is Accident_severity which is a multi-class variable. 
@@ -119,7 +115,7 @@ The task is to classify this variable based on the other 31 features step-by-ste
 The metric for evaluation will be f1-score
 """)
 
-st.markdown("Please find GitHub repository link of project: [Click Here](https://github.com/avikumart/Road-Traffic-Severity-Classification-Project)")                  
+st.markdown("Please find GitHub repository link of project: [Click Here](https://github.com/Beloved-vince/RTA_prediction_system)")                  
    
 # run the main function               
 if __name__ == '__main__':
